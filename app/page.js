301da3092bd684d4b7edcdca0f79d5eab6454c24
Aariyana Sayani <aariyana.sayani@edu.sait.ca>
2024-12-03 12:38:39 -0700

    tesxt somminct sl3k

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see you hey lady r changes instantly. YP o purple things hey</li>
+          <li>Save and see you hey lady r changes gunky mukey instantly. YP o purple things hey</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
